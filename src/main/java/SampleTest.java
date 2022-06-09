093c3bccb95c90144a4eacc9c354e450ecb16404
ravitechbee100 <92898318+ravitechbee100@users.noreply.github.com>
2022-06-09 13:47:05 -0500

    Update SampleTest.java
    updated with the commet

--- src/main/java/SampleTest.java
+++ src/main/java/SampleTest.java
@@ -6,7 +6,7 @@ public class SampleTest {
 	public static void main(String[] args) {
 		
 		SampleTest s = new SampleTest();
-		
+		// just the comment
 		System.out.println(System.getProperty("user.dir"));
 		try {
 			System.out.println(s.basePath);
